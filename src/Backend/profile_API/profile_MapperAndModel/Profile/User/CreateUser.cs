528caa91e5fcece0dea1aac13e5965435929a1a9
a.samoshuk <a.samoshuk@rit.va>
2025-01-15 11:46:11 +0300

    refactor

--- src/Backend/profile_API/profile_MapperAndModel/Profile/User/CreateUser.cs
+++ src/Backend/profile_API/profile_MapperAndModel/Profile/User/CreateUser.cs
@@ -2,16 +2,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace profile_MapperModel.Profile.User;
 
+using System.ComponentModel.DataAnnotations;
+
 public class CreateUser
 {
-    [Required]
+    [Required(ErrorMessage = "Login is required.")]
     public string Login { get; set; } = string.Empty;
-    [Required]
+
+    [Required(ErrorMessage = "First name is required.")]
     public string FirstName { get; set; } = string.Empty;
-    [Required]
+
+    [Required(ErrorMessage = "Last name is required.")]
     public string LastName { get; set; } = string.Empty;
-    
+
     public string? AvatarUrl { get; set; }
-    [Required]
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
     public string Password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Confirm password is required.")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
--- src/Backend/profile_API/profile_MapperAndModel/Profile/User/LoginUser.cs
+++ src/Backend/profile_API/profile_MapperAndModel/Profile/User/LoginUser.cs
@@ -2,10 +2,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace profile_MapperModel.Profile.User;
 
+using System.ComponentModel.DataAnnotations;
+
 public class LoginUser
 { 
-    [Required]
+    [Required(ErrorMessage = "Login is required.")]
+    [StringLength(100, ErrorMessage = "Login must be between 3 and 100 characters.", MinimumLength = 3)]
     public string Login { get; set; } = string.Empty;
-    [Required]
+
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
+    [StringLength(100, ErrorMessage = "Password must be between 6 and 100 characters.", MinimumLength = 6)]
     public string Password { get; set; } = string.Empty;
 }
